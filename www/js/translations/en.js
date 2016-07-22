fb450c3785a43c6ecd3b596f20f97b23f2be786d
Ævar Eggertsson <aevar.eggertsson@systek.no>
2016-07-22 00:47:43 +0000

    English translations fixes, sublime added to gitignore

--- www/js/translations/en.js
+++ www/js/translations/en.js
@@ -1,37 +1,37 @@
 define('translations/en', 
-	[], 
-	function(){
-	'use strict';
+    [], 
+    function(){
+    'use strict';
 
-	return {
-		lang: 'en',
-		langName: 'English',
-		compatibilityJSON: 'v2',
-		translation: {
-			'dashboard': {
-				'lastweek': 'Workouts last week',
-				'lastweek_plural': 'Workouts last {{value}} weeks',
-				'weeknum': 'Week number'
-			},
-			'shared': {
+    return {
+        lang: 'en',
+        langName: 'English',
+        compatibilityJSON: 'v2',
+        translation: {
+            'dashboard': {
+                'lastweek': 'Workouts last week',
+                'lastweek_plural': 'Workouts last {{value}} weeks',
+                'weeknum': 'Week number'
+            },
+            'shared': {
                 'name': 'Name',
-				'select': 'Select one',
-				'close': 'Close',
-				'back': 'Back',
-				'continue': 'Continue',
+                'select': 'Select one',
+                'close': 'Close',
+                'back': 'Back',
+                'continue': 'Continue',
                 'next': 'Next',
-				'complete': 'Complete',
-				'save': 'Save',
-				'cancel': 'Cancel',
-				'ok': 'Ok',
-				'date': 'Date',
-				'comment': 'Comment',
+                'complete': 'Complete',
+                'save': 'Save',
+                'cancel': 'Cancel',
+                'ok': 'Ok',
+                'date': 'Date',
+                'comment': 'Comment',
                 'sek': 'sek',
                 'min': 'min',
                 'enabled': 'Enabled',
                 'toggledisabled': 'Toggle disabled',
                 'description': 'Description'
-			},
+            },
             'measure': {
                 'title': 'Measurements',
                 'measure': 'Measure',
@@ -55,67 +55,67 @@ define('translations/en',
                 'exercises': 'Exercises',
                 'selectormake': 'Select or make your own',
                 'estmin': 'Estimated time in minutes',
-                'addset': 'Bæta við setti',
+                'addset': 'Add set',
                 'addexercise': 'Add exercise',
                 'addsession': 'Add session',
                 'get': 'Get more..',
                 'setcurr': 'Set as current',
                 'current': 'This is your current workout plan'
             },
-			'workout': {
-				'reps': 'Reps',
-				'set': 'Set',
-				'weight': 'Weight',
-				'freerun': 'Free-run',
-				'freeruntext': 'Not following the plan? That\'s no problem!',
-				'log': 'Log',
-				'goodjob': 'Good job!',
-				'youlifted': 'You lifted',
-				'muscle': 'Muscle',
-				'movement': 'Movement',
-				'addset': 'Add set',
+            'workout': {
+                'reps': 'Reps',
+                'set': 'Set',
+                'weight': 'Weight',
+                'freerun': 'Free-run',
+                'freeruntext': 'Not following the plan? That\'s no problem!',
+                'log': 'Log',
+                'goodjob': 'Good job!',
+                'youlifted': 'You lifted',
+                'muscle': 'Muscle',
+                'movement': 'Movement',
+                'addset': 'Add set',
                 'addexercise': 'Add exercise',
-				'musclegroup': 'Muscle group',
+                'musclegroup': 'Muscle group',
                 'setsandpause': 'Sets & Pause'
-			},
-			'workouttype': {
-				'type': 'Type',
-				'weightrep': 'Weight/Reps',
-				'weight': 'Weight',
-				'reps': 'Repetitions',
-				'distance': 'Distance',
-				'time': 'Time',
-				'beats': 'Heartrate'
-			},
-			'gender': {
-				'gender': 'Kyn',
-				'male': 'Karlmaður',
-				'female': 'Kona'
-			},
-			'settings': {
-				'title': 'Settings',
-				'age': 'Age',
-				'years': 'years',
-				'goal': 'I want to..',
-				'buildmuscle': 'Build muscles',
-				'looseweight': 'Loose weight',
-				'gethealthy': 'Get healthier',
-				'weightgoal': 'Do you have a weight goal?',
-				'unit': 'Units of measure',
-				'appcolor': 'App colors',
-				'blue': 'Blue',
-				'red': 'Red',
-				'orange': 'Orange',
-				'pantone': 'Pantone',
-				'language': 'Language'
-			},
-			'about': {
-				'text': 'Styrkur is icelandic for strength. And that is just what this app is for.',
-				'site': 'Visit us!',
-				'thirdparty': '3rd party',
-				'thanks': 'Special thanks to all the free and open-source software that this app is built on top of. It would not be as good if it was not for them.'
-			}
-		}
-	};
+            },
+            'workouttype': {
+                'type': 'Type',
+                'weightrep': 'Weight/Reps',
+                'weight': 'Weight',
+                'reps': 'Repetitions',
+                'distance': 'Distance',
+                'time': 'Time',
+                'beats': 'Heartrate'
+            },
+            'gender': {
+                'gender': 'Kyn',
+                'male': 'Male',
+                'female': 'Female'
+            },
+            'settings': {
+                'title': 'Settings',
+                'age': 'Age',
+                'years': 'years',
+                'goal': 'I want to..',
+                'buildmuscle': 'Build muscles',
+                'looseweight': 'Loose weight',
+                'gethealthy': 'Get healthier',
+                'weightgoal': 'Do you have a weight goal?',
+                'unit': 'Units of measure',
+                'appcolor': 'App colors',
+                'blue': 'Blue',
+                'red': 'Red',
+                'orange': 'Orange',
+                'pantone': 'Pantone',
+                'language': 'Language'
+            },
+            'about': {
+                'text': 'Styrkur is icelandic for strength. And that is just what this app is for.',
+                'site': 'Visit us!',
+                'thirdparty': '3rd party',
+                'thanks': 'Special thanks to all the free and open-source software that this app is built on top of. It would not be as good if it was not for them.'
+            }
+        }
+    };
 
 });
