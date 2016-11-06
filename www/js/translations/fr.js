9d3a51182621c2a13fde0471472dfd3b914e9af3
Anne017 <anneonyme017@openmailbox.org>
2016-11-06 23:42:39 +0100

    French translation added (#4)
    * French translation added

---
+++ www/js/translations/fr.js
@@ -0,0 +1,121 @@
+define('translations/fr', 
+    [], 
+    function(){
+    'use strict';
+
+    return {
+        lang: 'fr',
+        langName: 'Français',
+        compatibilityJSON: 'v2',
+        translation: {
+            'dashboard': {
+                'lastweek': 'Entraînements cette semaine',
+                'lastweek_plural': 'Entraînements des {{value}} dernières semaines',
+                'weeknum': 'Numéro de semaine'
+            },
+            'shared': {
+                'name': 'Nommer',
+                'select': 'Sélectionner',
+                'close': 'Fermer',
+                'back': 'Retour',
+                'continue': 'Continuer',
+                'next': 'Suivant',
+                'complete': 'Compléter',
+                'save': 'Enregistrer',
+                'cancel': 'Annuler',
+                'ok': 'Ok',
+                'date': 'Date',
+                'comment': 'Commentaire',
+                'sek': 'sec',
+                'min': 'min',
+                'enabled': 'Activé',
+                'toggledisabled': 'Interrupteur désactivé',
+                'description': 'Description'
+            },
+            'measure': {
+                'title': 'Mesures',
+                'measure': 'Mesure',
+                'addpart': 'Ajouter un élément',
+                'unittype': 'Type d\'unité',
+                'weight': 'Poids',
+                'percent': 'Pourcentage',
+                'shortlen': 'Courte durée',
+                'longlen': 'Longue durée',
+                'add': 'Ajouter',
+                'nomeasure': 'Aucune mesure ne semble enregistrée. Voulez-vous en enregistrer une maintenant ?',
+                'minnotmet': 'Deux mesures minimum sont nécessaires',
+                'lastwas': 'La dernière était {{lastOne}} à {{lastDate}}'
+            },
+            'create': {
+                'edit': 'Modifier',
+                'planner': 'Calendrier d\'entraînements',
+                'create': 'Créer un programme',
+                'sessionname': 'Nom de la session',
+                'exercise': 'Exercice',
+                'exercises': 'Exercices',
+                'selectormake': 'Sélectionnez ou faites le vôtre',
+                'estmin': 'Temps estimé en minutes',
+                'addset': 'Ajouter une série',
+                'addexercise': 'Ajouter un exercice',
+                'addsession': 'Ajouter une session',
+                'get': 'Voir plus...',
+                'setcurr': 'Définir comme programme actuel',
+                'current': 'Ceci est votre programme d\'entraînement actuel'
+            },
+            'workout': {
+                'reps': 'Répétitions',
+                'set': 'Série',
+                'weight': 'Poids',
+                'freerun': 'Libre',
+                'freeruntext': 'Vous ne suivez pas le plan ? Ce n\'est pas un problème !',
+                'log': 'Journal',
+                'goodjob': 'Beau boulot !',
+                'youlifted': 'Vous avez soulevé',
+                'muscle': 'Muscle',
+                'movement': 'Mouvement',
+                'addset': 'Ajouter une série',
+                'addexercise': 'Ajouter un exercice',
+                'musclegroup': 'Groupe de muscles',
+                'setsandpause': 'Séries & Pause'
+            },
+            'workouttype': {
+                'type': 'Type',
+                'weightrep': 'Poids/Répétitions',
+                'weight': 'Poids',
+                'reps': 'Répétitions',
+                'distance': 'Distance',
+                'time': 'Temps',
+                'beats': 'Fréquence cardiaque'
+            },
+            'gender': {
+                'gender': 'Sexe',
+                'male': 'Homme',
+                'female': 'Femme'
+            },
+            'settings': {
+                'title': 'Paramètres',
+                'age': 'Âge',
+                'years': 'ans',
+                'goal': 'Je veux...',
+                'buildmuscle': 'Prendre du muscle',
+                'looseweight': 'Perdre du poids',
+                'gethealthy': 'Être en meilleure santé',
+                'weightgoal': 'Avez-vous un objectif de poids ?',
+                'unit': 'Unités de mesure',
+                'appcolor': 'Couleurs de l\'application',
+                'blue': 'Bleue',
+                'red': 'Rouge',
+                'orange': 'Orange',
+                'pantone': 'Nuancier',
+                'language': 'Langue'
+            },
+            'about': {
+                'text': 'Styrkur est un terme islandais qui signifie « force ». Et c\'est exactement ce pourquoi cette application est faite.',
+                'site': 'Rendez-nous visite !',
+                'thirdparty': 'Logiciels tiers',
+                'thanks': 'Merci en particulier à tous les logiciels libres et open source sur lesquels sont basés cette application. Elle ne serait pas aussi aboutie sans eux.'
+            }
+        }
+    };
+
+});
